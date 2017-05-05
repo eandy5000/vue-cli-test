cfa0761a845299c3cf1be1d663742680aa0299db
Andy Eccleston <eandy5000@yahoo.com>
2017-05-05 09:17:38 -0500

    added express server to vue-cli project for development

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const Path = require('path')
+const fs = require('fs')
+const app = express()
+
+app.set('port', (process.env.PORT || 4000))
+
+let template = fs.readFileSync(Path.resolve('dist', 'index.html'), 'utf-8')
+
+app.get('/', function (req, res) {
+  res.send(template)
+})
+
+app.use(express.static(Path.join(__dirname, 'dist')))
+
+app.listen(app.get('port'), function () {
+  console.log(`listening on port: ${app.get('port')}`)
+})
+
+
+
+
